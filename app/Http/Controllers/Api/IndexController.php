b2abcd15d8644c9ab0289a861d7a4426f3d73a09
weidada <ingwei@163.com>
2025-09-15 14:40:16 +0800

    8.x

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -5,12 +5,11 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\JsonResource;
 use Jiannei\Response\Laravel\Support\Facades\Response;
 
 class IndexController extends ApiBaseController
 {
-    public function test(Request $request): JsonResponse|JsonResource
+    public function test(Request $request): JsonResponse
     {
         // Response::ok();
         // Response::fail();
